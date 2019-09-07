e04efe2cf16cc7564785f73e7a2f4496c71386da
admin <admin@DESKTOP-J73EDB0>
2019-09-07 13:09:58 +0530

    My First commit

---
+++ CucumberRestAssured/src/test/java/com/basic/postStringJsonSD/RunMyPostFromTextFileTest.java
@@ -0,0 +1,30 @@
+package com.basic.postStringJsonSD;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+	
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		
+		features= {"src/test/resources/com/basic/postStringJsonFF/"},
+		glue={"com/basic/postStringJsonSD/"},
+		
+		monochrome=true,
+		dryRun=false,
+		plugin={"pretty",
+				"html:target/cucumber-htmlreport",
+				"json:target/cucumber-report774.json",
+				"com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport1.html"
+		}
+		
+		)
+		
+
+public class RunMyPostFromTextFileTest {
+
+
+}
